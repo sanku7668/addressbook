3d50fb63d2397e07753ba5145cf31d3b1228ff3e
Sanket <sanketjambharunkar@gmail.com>
2024-07-04 17:27:07 +0530

    3rd commit

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ec2_instance_type" {
   description = "The EC2 instance type"
   type        = string
-  default     = "t2.micro"  # Change this to your desired instance type
+  default     = "t2.medium"  # Change this to your desired instance type
 }
